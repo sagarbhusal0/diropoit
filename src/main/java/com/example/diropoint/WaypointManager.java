783c0070f3a8955aa79f85ddc1a5a0d8d7176140
sagar bhusal <sagar@sagarb.com>
2025-06-06 14:49:50 +0545

    jcc

--- src/main/java/com/example/diropoint/WaypointManager.java
+++ src/main/java/com/example/diropoint/WaypointManager.java
@@ -3,7 +3,6 @@ package com.example.diropoint;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.reflect.TypeToken;
-import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.dimension.DimensionType;
 
 import java.io.File;
